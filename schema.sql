f08d23f506813d488606dcc1113515621f03077f
yasinabdmahmood <abd181103@gmail.com>
2022-10-24 18:22:46 +0300

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;       
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animal(
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(250),
+date_of_birth DATE,
+escape_attempts INT,
+neutered BOOLEAN,
+weight_kg DECIMAL (4, 2));
+-- CREATE TABLE animals (
+--     name varchar(100)
+-- );
+
